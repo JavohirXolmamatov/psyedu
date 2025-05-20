cd1db28723b026e193a24f335299e8f50a10b2de
kenjayev <kenjayevazizbek2oo4@gmail.com>
2025-05-20 11:07:53 +0500

    education seciton

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.psyedu.uz/", // sizga berilgan API url
+  baseURL: "https://testpsyedu.limsa.uz/", // sizga berilgan API url
   headers: {
     "Content-Type": "application/json",
   },
